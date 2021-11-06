7994b8dd7c64b417ebfa53dcade5268e366035f9
Bryanna Vinge <bevinge711@gmail.com>
2021-11-06 09:41:40 -0500

    working on build of model

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,10 @@
+const router = require('express').Router();
+const Workout = require("../models/")
+
 //post new workout
 
 //put update a current workout
 
 //get view workout
+
+//get workout range
---
+++ models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Please enter your name"
+    },
+    type: {
+        type: String,
+        required: "Select a Workout Type"
+    },
+    weight: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    durations: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    },
+})
